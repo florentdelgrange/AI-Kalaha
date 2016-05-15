0c3f8d8a882e61d3a234dcbfcb9ee57cf11ff5f2
radioGiorgio <florent.delgrange@gmail.com>
2016-05-15 20:54:59 +0200

    Implementation of the AI that minimizes the kalaha of other players

---
+++ src/Games/Kalaha/Players/KahalaMinimizerAI.java
@@ -0,0 +1,45 @@
+package Games.Kalaha.Players;
+
+import Games.Kalaha.Boards.Board;
+import Games.Kalaha.Game;
+import Games.Kalaha.Move;
+import Games.Kalaha.Players.AI.Heuristic;
+import Games.Kalaha.Players.AI.MaxN;
+import Games.Kalaha.Players.AI.Minimax;
+import Games.Kalaha.Players.AI.Utility;
+
+import java.util.HashMap;
+
+/**
+ * Created by florentdelgrange on 15/05/16.
+ */
+public class KahalaMinimizerAI extends Player{
+    @Override
+    public Move pickMove(String s) {
+        Utility utility = new Utility() {
+            @Override
+            public Double getScore(Board board, String player) {
+                HashMap<String, Integer> sums = board.getSums(true, false);
+                return -1.0 * ( sums.values().stream().reduce(0, (a, b) -> a + b) - sums.get(player) );
+            }
+
+            @Override
+            public Game.LeftTokensGrantee getLeftTokensGrantee() {
+                return leftTokensGrantee;
+            }
+
+            @Override
+            public boolean getEmptyCapture() {
+                return emptyCapture;
+            }
+        };
+        Heuristic heuristic;
+        if(players.size() == 2) {
+            heuristic = new Minimax(7, utility, players, s);
+        }
+        else {
+            heuristic = new MaxN(3, utility, players, s);
+        }
+        return new Move(heuristic.compute(board));
+    }
+}
